d356352e1567466a7d1bd69002c7e17d5ed18314
ThaUnknown <kapi.skowronek@gmail.com>
2021-05-04 08:09:44 +0200

    update

--- sw.js
+++ sw.js
@@ -1,60 +1,77 @@
-// Activate event
-// Be sure to call self.clients.claim()
-self.addEventListener('activate', evt =>  {
-	// `claim()` sets this worker as the active worker for all clients that
-	// match the workers scope and triggers an `oncontrollerchange` event for
-	// the clients.
-	return self.clients.claim()
+self.addEventListener('install', e => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  return self.clients.claim()
 })
 
 self.addEventListener('fetch', evt => {
   const { request } = evt
-  const { url, method } = request
-  const headers = [...request.headers]
+  const { url, method, headers } = request
   if (!url.includes(self.registration.scope + 'webtorrent/')) return null
 
-  function getConsumer(clients) {
-    return new Promise((rs, rj) => {
+  function getConsumer (clients) {
+    return new Promise(resolve => {
       // Use race condition for whoever controls the response stream
       for (const client of clients) {
         const mc = new MessageChannel()
-        mc.port1.onmessage = evt => rs([evt.data, mc])
+        const { port1, port2 } = mc
+        port1.onmessage = evt => {
+          resolve([evt.data, mc])
+        }
         client.postMessage({
           url,
           method,
-          headers,
+          headers: [...headers],
           scope: self.registration.scope
-        }, [mc.port2])
+        }, [port2])
       }
     })
   }
 
   evt.respondWith(
     clients.matchAll({ type: 'window', includeUncontrolled: true })
-    .then(getConsumer)
-    .then(([data, consumer]) => {
-      const readable = 'body' in data ? data.body : new ReadableStream({
-        pull(controller) {
-          console.log('requesting data')
-          return new Promise(rs => {
-            consumer.port1.onmessage = evt => {
-              evt.data
-                ? controller.enqueue(evt.data) // evt.data is Uint8Array
-                : controller.close() // evt.data is null, means the stream ended
-              rs()
-            }
-            consumer.port1.postMessage(true) // send a pull request
-          })
-        },
-        cancel() {
-          // This event is never executed
-          console.log('request aborted')
-          consumer.port1.postMessage(false) // send a cancel request
-        }
-      })
+      .then(getConsumer)
+      .then(([data, mc]) => {
+        let tm = null
+        const body = data.body === 'stream'
+          ? new ReadableStream({
+              pull (controller) {
+                return new Promise(resolve => {
+                  mc.port1.onmessage = evt => {
+                    if (evt.data) {
+                      controller.enqueue(evt.data) // evt.data is Uint8Array
+                    } else {
+                      clearTimeout(tm)
+                      controller.close() // evt.data is null, means the stream ended
+                      mc.port1.onmessage = null
+                    }
+                    resolve()
+                  }
+                  // 'media player' does NOT signal a close on the stream and we cannot close it because it's locked to the reader,
+                  // so we just empty it after 5s of inactivity,
+                  // the browser will request another port anyways
+                  clearTimeout(tm)
+                  tm = setTimeout(() => {
+                    controller.close()
+                    mc.port1.postMessage(false) // send timeout
+                    mc.port1.onmessage = null
+                    resolve()
+                  }, 5000)
 
-      return new Response(readable, data)
-    })
-    .catch(console.error)
+                  mc.port1.postMessage(true) // send a pull request
+                })
+              },
+              cancel () {
+                // This event is never executed
+                mc.port1.postMessage(false) // send a cancel request
+              }
+            })
+          : data.body
+
+        return new Response(body, data)
+      })
+      .catch(console.error)
   )
 })
